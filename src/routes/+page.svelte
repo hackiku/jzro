15fca9c6963e2d545e7b5efa00cab8b9bd64bfcb
hackiku <ivan@pipewriter.io>
2024-01-10 13:30:09 +0100

    copy config from other darkmode project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from 'svelte';
+
+</script>
+
+<div class="container mx-auto px-4">
+  <header class="flex justify-between items-center py-4">
+    <h1 class="text-md font-bold text-white">jzro.co</h1>
+    <nav>
+      <ul class="flex space-x-4">
+    <li><a href="#" class="text-white hover:text-gray-300">Work</a></li>
+        <li><a href="#" class="text-white hover:text-gray-300">Play</a></li>
+        <li><a href="#" class="text-white hover:text-gray-300">About</a></li>
+      </ul>
+    </nav>
+  </header>
+</div>
+  <!-- hero -->
+  <section class="hero mt-8 py-8 flex justify-center items-center">
+    <div class="max-w-md text-center">
+      <h2 class="text-4xl font-bold text-white mb-4">Space Race FOMO relief in 100 words a pop</h2>
+      <p class="text-xl text-gray-500 mb-8">Space industry barometer. Never miss a beat with AI news summarized in 100 words.</p>
+      <form class="flex justify-center items-center">
+        <input type="email" placeholder="email away" class="border border-gray-300 rounded-l px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-300" />
+        <button class="bg-brandBlue rounded-r text-white px-4 py-2 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300">Subscribe</button>
+      </form>
+    </div>
+  </section>
+  
+
+<footer class="text-center">footer</footer>
+
+<style>
+  /* Add additional styling as needed */
+</style>
