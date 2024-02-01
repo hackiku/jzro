f0399f05b8f25dbbd27174d0926eeb1f341ad96e
hackiku <ivan@pipewriter.io>
2024-02-01 01:55:33 +0100

    abstract navbar into component

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from 'svelte';
+</script>
+
+<div class="container mx-auto px-4">
+    <header class="flex justify-between items-center py-4 px-20
+    ">
+      <a class="text-md font-mono"  href="/">🚁 jzro</a>
+      <nav>
+        <ul class="flex space-x-4 items-center relative">
+          <li><a href="/fiddle" class="text-white hover:text-blue-500">fiddle</a></li>
+          <li><a href="/orbit-test" class="text-white hover:text-blue-500">orbit</a></li>
+          <li><a href="/cta" class="text-white hover:text-blue-500">cta</a></li>
+          <li><a href="/grav-sym" class="text-[#F21D26] hover:text-blue-500">grav</a></li>
+          <li><a href="/mars-metar" class="text-white hover:text-blue-500">jzro</a></li>
+        </ul>
+      </nav>
+    </header>
+  </div>
