bc845b62f0bfb0454bdf1dfec053cf00028f459c
hackiku <ivan@pipewriter.io>
2024-02-14 15:35:17 +0100

    ballistic projectile with planet ready, no gravity yet

--- src/routes/boing-relative-dashed-ok/+page.svelte
+++ src/routes/boing-relative-dashed-ok/+page.svelte
@@ -2,13 +2,21 @@
 <script>
   import { onMount } from 'svelte';
   import { browser } from '$app/environment'; // Import browser variable
+  import gsap from 'gsap';
+  
+  let MotionPathPlugin;
 
   let velocity = 30; // Initial velocity
-  let trajectoryPath = ''; // Initialize empty string for the path
+  let trajectoryPath = '';
+  let rocketLaunched = false; // State to control rocket animation
+
+  // const width = window.innerWidth;
+  // const height = window.innerHeight;
+
 
   function calculateTrajectory(velocity, width, height) {
-    const startX = width * 0.01; // 5% from the left of the viewport
-    const startY = height * 1; // Adjusted for the rocket's position
+    const startX = width * 0.01;
+    const startY = height * 1;
 
     const heightModifier = (velocity / 50) * height * 0.8;
     const lengthModifier = (velocity / 50) * width * 1.6;
@@ -19,22 +27,55 @@
     return `M${startX},${startY} C${startX + totalLength / 4},${peakHeight} ${startX + 3 * totalLength / 4},${peakHeight} ${totalLength},${startY}`;
   }
 
-  // Only run this code client-side, after the component has mounted
-  if (browser) {
-    onMount(() => {
+
+
+
+if (browser) {
+
+  onMount(async () => {
+    if (typeof window !== 'undefined') {
+      MotionPathPlugin = (await import('gsap/MotionPathPlugin')).MotionPathPlugin;
+      gsap.registerPlugin(MotionPathPlugin);
+
       const width = window.innerWidth;
       const height = window.innerHeight;
-
       trajectoryPath = calculateTrajectory(velocity, width, height);
 
-      // Recalculate path on viewport resize
       window.addEventListener('resize', () => {
         trajectoryPath = calculateTrajectory(velocity, window.innerWidth, window.innerHeight);
       });
-    });
-  }
+    }
+  });
+};
 
-  // Reactive statement to update the path when velocity changes, guarded for client-side execution
+  function launchRocket() {
+  // Create a copy of the rocket
+  const rocketCopy = document.createElement('img');
+  rocketCopy.src = 'game/bananica.png';
+  rocketCopy.classList.add('rocket');
+  rocketCopy.style.position = 'absolute';
+  rocketCopy.style.width = '12em';
+  rocketCopy.style.top = '80vh'; // Adjust based on the original rocket's position
+  rocketCopy.style.left = '5vw'; // Adjust based on the original rocket's position
+  document.body.appendChild(rocketCopy);
+
+  gsap.to(rocketCopy, {
+    duration: 80 / velocity,
+    ease: "power1.inOut",
+    motionPath: {
+      path: trajectoryPath, // Use the dynamic path
+      align: "self",
+      // autoRotate: true,
+      alignOrigin: [0, 0],
+    },
+    onComplete: () => {
+      rocketCopy.remove();
+    },
+  });
+}
+
+
+  // make sure client-side execution only
   $: if (browser && velocity) {
     trajectoryPath = calculateTrajectory(velocity, window.innerWidth, window.innerHeight);
   }
@@ -42,10 +83,10 @@
 
 
 
-<div class=" flex-col items-center justify-start h-screen">
+<section class=" flex-col items-center justify-start h-screen">
   <div class="relative w-full h-full">
     <!-- Rocket positioned at a fixed point -->
-    <img src="game/rocket.png" alt="rocket" class="absolute" style="left: 5vw; bottom: 20vh;" />
+    <img src="game/bananica.png" alt="rocket" class="rocket" />
     
     <!-- Corrected Trajectory SVG, now with the correct stroke color -->
     <svg class="absolute" width="100vw" height="100vh" style="position: absolute; bottom: 20vh; left: 5vw;" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -62,20 +103,27 @@
 
   <!-- Velocity slider -->
   <div class="z-50 fixed p-6 bottom-12 rounded-full hover:backdrop-blur-md flex justify-center items-center bg-gray-500 bg-opacity-10 left-1/2 transform -translate-x-1/2">
-    <button class="text-4xl">💥</button>
-    <div class="relative flex flex-col justify-center group hover:opacity-100 transition-opacity">
-      <div class="flex flex-row items-center justify-center">
-        <p>v = <span class="velocity">{velocity}</span> · 10^6 m/s</p>
-      </div>
-      <!-- Slider -->
+    <button class="text-4xl" on:click={launchRocket}>💥</button>
+    <div class="relative space-between-12 flex flex-col justify-center group hover:opacity-100 transition-opacity">
+        <p>v = <span class="velocity">{velocity}</span> m/s</p>
       <input type="range" min="1" max="100" value={velocity} class="slider w-full" id="particleNumber" on:input={(e) => velocity = e.target.value} />
     </div>
   </div>
-</div>
+  <img src="assets/footer.svg" alt="" class="absolute inset-x-0 bottom-0 w-full z-0 mb-0">
+</section>
+
 
 <style>
-  @keyframes shoot {
-    to { transform: translate(80vw, -80vh); }
+  .rocket {
+    position: absolute;
+    top: 80vh; 
+    left: 5vw;
+    transform: translateX(-20%);
+    width: 20em;
+    z-index: 10;
+  }
+  .original {
+    /* Additional styles for the original rocket, if needed */
   }
   .velocity {
     color: #ff3d00;
--- src/routes/boing/+page.svelte
+++ src/routes/boing/+page.svelte
@@ -2,14 +2,18 @@
 <script>
   import { onMount } from 'svelte';
   import { browser } from '$app/environment'; // Import browser variable
+  // import { navigate } from 'svelte-routing';
   import gsap from 'gsap';
+  import Planet from '$lib/Planet.svelte';
   
   let MotionPathPlugin;
 
-  let velocity = 30; // Initial velocity
+  let velocity = 26; // Initial velocity
   let trajectoryPath = '';
   let rocketLaunched = false; // State to control rocket animation
 
+  let hitTarget = false;
+  let hitMessage = 'Srecan Svetski Trifun i hvala, uvek, na tough love';
 
   function calculateTrajectory(velocity, width, height) {
     const startX = width * 0.01;
@@ -23,8 +27,6 @@
 
     return `M${startX},${startY} C${startX + totalLength / 4},${peakHeight} ${startX + 3 * totalLength / 4},${peakHeight} ${totalLength},${startY}`;
   }
-
-
 
 
 if (browser) {
@@ -44,54 +46,65 @@ if (browser) {
     }
   });
 };
-  // Only run this code client-side, after the component has mounted
-  // if (browser) {
-  //   onMount(() => {
-      
-  //     const width = window.innerWidth;
-  //     const height = window.innerHeight;
-
-  //     gsap.registerPlugin(MotionPathPlugin) 
-
-  //     trajectoryPath = calculateTrajectory(velocity, width, height);
-
-  //     // Recalculate path on viewport resize
-  //     window.addEventListener('resize', () => {
-  //       trajectoryPath = calculateTrajectory(velocity, window.innerWidth, window.innerHeight);
-  //     });
-  //   });
-  // }
-
-
 
   function launchRocket() {
-    if (!rocketLaunched) { // Prevent multiple animations simultaneously
-      rocketLaunched = true;
-      gsap.to('.rocket', {
-        duration: 5,
-        ease: "power1.inOut",
-        motionPath: {
-          path: "M8.74,730 C220.685,379.59999999999997 644.575,379.59999999999997 847.78,730",
-          align: "self",
-          autoRotate: true,
-          alignOrigin: [1, 0],
-        },
-        onComplete: () => rocketLaunched = false, // Reset the animation state
-      });
-    }
-  }
+  // Create a copy of the rocket
+  const rocketCopy = document.createElement('img');
+  rocketCopy.src = 'game/bananica.png';
+  rocketCopy.classList.add('rocket');
+  rocketCopy.style.position = 'absolute';
+  rocketCopy.style.width = '9em';
+  rocketCopy.style.top = '80vh'; // Adjust based on the original rocket's position
+  rocketCopy.style.left = '5vw'; // Adjust based on the original rocket's position
+  document.body.appendChild(rocketCopy);
 
-  // Reactive statement to update the path when velocity changes, guarded for client-side execution
+  gsap.to(rocketCopy, {
+    duration: 80 / velocity,
+    ease: "power1.inOut",
+    motionPath: {
+      path: trajectoryPath, // Use the dynamic path
+      align: "self",
+      // autoRotate: true,
+      alignOrigin: [0, 0],
+    },
+    
+    onUpdate: () => {
+      // Check for collision during the animation
+      const rocketRect = rocketCopy.getBoundingClientRect();
+      const targetRect = document.querySelector('svg circle').getBoundingClientRect();
+
+      // Simple overlap check
+      if (rocketRect.right > targetRect.left && rocketRect.left < targetRect.right &&
+          rocketRect.bottom > targetRect.top && rocketRect.top < targetRect.bottom) {
+        hitTarget = true;
+        gsap.killTweensOf(rocketCopy); // Stop the animation
+        rocketCopy.remove(); // Remove the rocket
+      }
+    },
+    onComplete: () => {
+      if (!hitTarget) { // If target was not hit
+        rocketCopy.remove();
+      }
+    },
+  });
+}
+
+
+  // make sure client-side execution only
   $: if (browser && velocity) {
     trajectoryPath = calculateTrajectory(velocity, window.innerWidth, window.innerHeight);
   }
 </script>
 
 
-<div class=" flex-col items-center justify-start h-screen">
+<!-- ========================================= -->
+
+
+
+<section class="flex-col items-center justify-start h-screen">
   <div class="relative w-full h-full">
     <!-- Rocket positioned at a fixed point -->
-    <img src="game/rocket.png" alt="rocket" class="absolute rocket" />
+    <img src="game/bananica.png" alt="rocket" class="rocket" />
     
     <!-- Corrected Trajectory SVG, now with the correct stroke color -->
     <svg class="absolute" width="100vw" height="100vh" style="position: absolute; bottom: 20vh; left: 5vw;" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -101,31 +114,51 @@ if (browser) {
       
   <div>
     <!-- Target area -->
-    <svg class="absolute" style="right: 10vw; top: 10vh;" width="100" height="100">
-      <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
-    </svg>
+    <div class="absolute" style="right: 10vw; top: 10vh;" width="100" height="100">
+      <Planet initialGravity={5} color="#F4191D" label="Jao Bre" />
+    </div> 
+    <!-- <svg class="absolute" style="right: 10vw; top: 10vh;" width="100" height="100"> -->
+      <!-- <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" /> -->
+      <!-- <text x="50" y="55" font-family="Times" font-size="1.6em" fill="white" text-anchor="middle" dominant-baseline="central">jao bre</text> -->
+    <!-- </svg> -->
   </div>
 
   <!-- Velocity slider -->
-  <div class="z-50 fixed p-6 bottom-12 rounded-full hover:backdrop-blur-md flex justify-center items-center bg-gray-500 bg-opacity-10 left-1/2 transform -translate-x-1/2">
-    <button class="text-4xl" on:click={launchRocket}>💥</button>
-    <div class="relative flex flex-col justify-center group hover:opacity-100 transition-opacity">
-      <div class="flex flex-row items-center justify-center">
-        <p>v = <span class="velocity">{velocity}</span> · 10^6 m/s</p>
-      </div>
-      <!-- Slider -->
+  <div class="z-50 fixed p-6 bottom-6 rounded-full backdrop-blur-md flex justify-center items-center bg-gray-500 bg-opacity-10 left-1/2 transform -translate-x-1/2">
+    <button class="text-5xl mr-6 hover:bg-red-500 rounded-full" on:click={launchRocket}>💥</button>
+    <div class="relative space-between-12 flex flex-col justify-center group hover:opacity-100 transition-opacity">
+        <p>v = <span class="velocity">{velocity}</span> m/s</p>
       <input type="range" min="1" max="100" value={velocity} class="slider w-full" id="particleNumber" on:input={(e) => velocity = e.target.value} />
     </div>
   </div>
-</div>
+  <img src="assets/footer.svg" alt="" class="fixed inset-x-0 bottom-0 w-full z-0 mb-0">
+
+
+  <!-- victory message -->
+  
+  {#if hitTarget}
+  <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-transparent p-4 rounded-2xl shadow-lg border-2" style="border-color: #F4191D;">
+    <div class="flex flex-col text-center mb-4">
+      <p class="text-center text-white text-xl mb-6">{hitMessage}</p>
+      <p class="text-4xl text center">🫀📚</p>
+      <a href="javascript:void(0);" onclick="window.location.reload();" class="text-[#F4191D] hover:text-red-700 mt-4 inline-block cursor-pointer" on:click={restartGame}>Restart →</a>
+      </div>
+  </div>
+  {/if}
+
+
+
+
+</section>
+
 
 <style>
   .rocket {
     position: absolute;
     top: 80vh; 
     left: 5vw;
-    transform: translateX(-50%);
-    width: 50px; 
+    transform: translateX(-20%);
+    width: 9em !important;
     z-index: 10;
   }
   .original {
