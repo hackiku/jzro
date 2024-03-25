83a218c542ac565ac41ec1f3590c2d42d9468772
hackiku <ivan@pipewriter.io>
2024-03-25 13:54:25 +0100

    solved damn footer

--- src/lib/ui/Footer.svelte
+++ src/lib/ui/Footer.svelte
@@ -5,16 +5,15 @@
   
   let width = 0; 
   let height = 70;
-  let atmoshpereOffset = 20;
+  let atmosphereOffset = 20;
 
   $: pathD = `M0,${height} Q${width / 2},0 ${width},${height} V${height} H0`;
+  // New path for the atmosphere layer
+  $: pathDAtmosphere = `M0,${height - atmosphereOffset} Q${width / 2},${-atmosphereOffset} ${width},${height - atmosphereOffset} V${height} H0`;
 
-  // Update the width on mount and when the window resizes
   onMount(() => {
     const updateWidth = () => {
       width = window.innerWidth;
-      // Optionally, adjust the height based on the width or other conditions
-      // height = calculateHeightBasedOnWidth(width);
     };
     
     window.addEventListener('resize', updateWidth);
@@ -27,23 +26,26 @@
 </script>
 
 <footer class="flex flex-col items-center justify-end w-full relative overflow-hidden">
-  <div class="w-full px-20 pb-14 flex justify-end space-x-4 z-20">
-    <a href="https://twitter.com" target="_blank" class="h-6 w-6"><img src="portfolio/tool-icons/figma.svg" alt="Twitter"></a>
-    <a href="https://facebook.com" target="_blank" class="h-6 w-6"><img src="portfolio/tool-icons/whatsapp.svg" alt="Facebook"></a>
-    <a href="https://instagram.com" target="_blank" class="h-6 w-6"><img src="portfolio/tool-icons/onshape.svg" alt="Instagram"></a>
+  
+  <div class="w-full px-12 md:pr-32 xl:pr-72 pb-20 flex justify-end space-x-4 z-20">
+    <a href="https://instagram.com" target="_blank" class="h-7 w-7 opacity-40 hover:opacity-100"><img src="ui/instagram-icon.svg" alt="Instagram"></a>
+    <a href="https://tiktok.com" target="_blank" class="h-7 w-7 opacity-40 hover:opacity-100"><img src="ui/tiktok-icon.svg" alt="TikTok"></a>
+    <a href="https://linkedin.com" target="_blank" class="h-7 w-7 opacity-40 hover:opacity-100"><img src="ui/linkedin-icon.svg" alt="LinkedIn"></a>
   </div>
   
-  <img src="ui/mcrn.svg" alt="MCRN flag" class="absolute left-[12vw] h-20 z-10">
+  <!-- <img src="ui/mcrn.svg" alt="MCRN flag" class="absolute left-[16vw] h-20 z-10"> -->
   
-  <svg class="w-full h-auto absolute bottom-0" viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg">
+  <svg class="w-full absolute bottom-0" viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg">
+    <path d={pathDAtmosphere} fill="#F21D26" opacity="0.2"/>
     <path d={pathD} fill="#F21D26"/>
-    <path opacity="0.1" d={pathD + 40} fill="#F21D26"/>
-    <path opacity="0.1" d="M557 0C201.913 0 0 75.1806 0 75.1806V120H1114V75.1806C1114 75.1806 912.088 0 557 0Z" fill="#F21D26"/>
   </svg>
-
-  <path opacity="0.1" d="M557 0C201.913 0 0 75.1806 0 75.1806V120H1114V75.1806C1114 75.1806 912.088 0 557 0Z" fill="#F21D26"/>
-
+  
+  <img src="ui/mcrn.svg" alt="MCRN flag" class="absolute left-[10vw] md:left-[38vh] h-20 z-10">
   <div class="w-full flex mb-1 md:mb-2 items-end justify-center z-20">
-    <p class="text-sm text-darkBg">Copyalright © 2024 jzro | Privacy | Terms</p>
+    <div class="text-sm text-darkBg">
+      Copyalright © 2024 jzro |
+      <a href="/privacy" target="_blank" class="mouse-pointer hover:underline hover:opacity-70">Privacy</a> |
+      <a href="/terms" target="_blank" class="mouse-pointer hover:underline hover:opacity-70">Terms</a> |
+    </div>
   </div>
 </footer>
